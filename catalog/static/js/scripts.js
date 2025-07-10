fa7d89f63130337a5f9c3b1603b662fc9a2457eb
itsnhyyy <22521038@gm.uit.edu.vn>
2025-07-10 19:52:56 +0700

    Django Tutorial Part 6: Generic listand detail views
    f

---
+++ catalog/static/js/scripts.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Trang đã tải xong!');
+});
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,35 @@
-# catalog/views.py
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import generic
+from catalog.models import Book, Author, BookInstance, Genre
+from catalog.constants import LoanStatus
 
 def index(request):
-    return HttpResponse("Welcome to the Catalog!")
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_instances_available = BookInstance.objects.filter(status__exact=LoanStatus.AVAILABLE.value).count()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10  
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+def book_detail_view(request, primary_key):
+    book = get_object_or_404(Book, pk=primary_key)
+    return render(request, 'catalog/book_detail.html', context={'book': book})
