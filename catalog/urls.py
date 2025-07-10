10373434db3a7723d3d4ee700a60527d52d3d360
itsnhyyy <22521038@gm.uit.edu.vn>
2025-07-10 19:52:56 +0700

    Django Tutorial Part 6: Generic list and detail views
    f

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
+
+app_name = 'catalog'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    # path('author/<int:pk>/', views.author_detail, name='author_detail'),
 ]
+
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -3,9 +3,12 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('i18n/', include('django.conf.urls.i18n')),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
