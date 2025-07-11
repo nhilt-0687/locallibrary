69d84e7b64ecd3f4cc5049fee65c71d1cb24cfd7
itsnhyyy <22521038@gm.uit.edu.vn>
2025-07-11 21:52:26 +0700

    Django Tutorial Part 8: User authentication and permissions
    f

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
 from . import views
+
+app_name = 'catalog'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('mybooks/', views.my_borrowed_books, name='my_borrowed_books'),
+    path(
+        'bookinstance/<uuid:bookinstance_id>/return/',
+        views.mark_book_returned,
+        name='mark_book_returned'
+    ),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,87 @@
-# catalog/views.py
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+from catalog.models import Book, Author, BookInstance
+from catalog.constants import LoanStatus, DEFAULT_PAGINATION_SIZE
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
+
 
 def index(request):
-    return HttpResponse("Welcome to the Catalog!")
+    """View function for home page of site."""
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(
+        status__exact=LoanStatus.AVAILABLE.value
+    ).count()
+    num_authors = Author.objects.count()
+
+    num_visits = request.session.get('num_visits', 0)
+    num_visits = num_visits + 1
+    request.session['num_visits'] = num_visits
+    request.session.modified = True
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(LoginRequiredMixin, generic.ListView):
+    model = Book
+    paginate_by = DEFAULT_PAGINATION_SIZE
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+def book_detail_view(request, primary_key):
+    book = get_object_or_404(Book, pk=primary_key)
+    book_instances = book.book_instance_set.select_related().all()
+
+    context = {
+        'book': book,
+        'book_instances': book_instances,
+    }
+    return render(
+        request,
+        'catalog/book_detail.html',
+        context=context
+    )
+
+
+@login_required
+def my_borrowed_books(request):
+    borrowed_books = BookInstance.objects.filter(
+        borrower=request.user,
+        status=LoanStatus.ON_LOAN.value
+    )
+    context = {
+        'borrowed_books': borrowed_books,
+        'LoanStatus': LoanStatus,
+    }
+    return render(request, 'catalog/my_borrowed_books.html', context=context)
+
+
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
+def mark_book_returned(request, bookinstance_id):
+    bookinstance = get_object_or_404(BookInstance, id=bookinstance_id)
+    if request.method == 'POST':
+        bookinstance.status = LoanStatus.AVAILABLE.value
+        bookinstance.borrower = None
+        bookinstance.due_date = None
+        bookinstance.save()
+    return redirect('catalog:my_borrowed_books')
