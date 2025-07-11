351181aeb37c061c1f864a99ba48a0e7b16d1fc4
itsnhyyy <22521038@gm.uit.edu.vn>
2025-07-11 21:52:26 +0700

    Django Tutorial Part 7: Session Framework
    f

--- catalog/models.py
+++ catalog/models.py
@@ -1,19 +1,35 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from .constants import MAX_LENGTH_NAME, MAX_LENGTH_AUTHOR_NAME, MAX_LENGTH_ISBN, MAX_LENGTH_SUMMARY, MAX_LENGTH_UNIQUE_ID
+from .constants import (
+    MAX_LENGTH_NAME,
+    MAX_LENGTH_AUTHOR_NAME,
+    MAX_LENGTH_ISBN,
+    MAX_LENGTH_SUMMARY,
+    LoanStatus
+)
 from django.urls import reverse
 import uuid
+
+
 class Genre(models.Model):
-    name = models.CharField(max_length=MAX_LENGTH_NAME, help_text=_("Enter the genre (e.g. Science Fiction)"))
-    
+    name = models.CharField(
+        max_length=MAX_LENGTH_NAME,
+        help_text=_("Enter the genre (e.g. Science Fiction)")
+    )
+
     def __str__(self):
         return self.name
 
+
 class Language(models.Model):
-    name = models.CharField(max_length=MAX_LENGTH_NAME, help_text=_("Enter the language (e.g. English)"))
-    
+    name = models.CharField(
+        max_length=MAX_LENGTH_NAME,
+        help_text=_("Enter the language (e.g. English)")
+    )
+
     def __str__(self):
         return self.name
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=MAX_LENGTH_AUTHOR_NAME)
@@ -30,13 +46,32 @@ class Author(models.Model):
     def get_absolute_url(self):
         return reverse('author-detail', args=[str(self.id)])
 
+
 class Book(models.Model):
     title = models.CharField(max_length=MAX_LENGTH_NAME)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
-    summary = models.TextField(max_length=MAX_LENGTH_SUMMARY, help_text=_("Enter a brief description of the book"))
-    isbn = models.CharField(_("ISBN"), max_length=MAX_LENGTH_ISBN, help_text=_("13 Character <a href='https://www.isbn-international.org/content/what-isbn'>ISBN number</a>"))
-    genre = models.ManyToManyField(Genre, help_text=_("Select a genre for this book"))
-    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(
+        max_length=MAX_LENGTH_SUMMARY,
+        help_text=_("Enter a brief description of the book")
+    )
+    isbn = models.CharField(
+        _("ISBN"),
+        max_length=MAX_LENGTH_ISBN,
+        help_text=_(
+            "13 Character "
+            "<a href='https://www.isbn-international.org/content/what-isbn'>"
+            "ISBN number</a>"
+        )
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        help_text=_("Select a genre for this book")
+    )
+    language = models.ForeignKey(
+        Language,
+        on_delete=models.SET_NULL,
+        null=True
+    )
 
     def __str__(self):
         return self.title
@@ -44,17 +79,45 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
 
+
 class BookInstance(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
     book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
     due_date = models.DateField(null=True, blank=True)
-    imprint = models.CharField(max_length=200, blank=True, null=True, help_text="Enter the publisher imprint")
-    status = models.CharField(max_length=20, choices=[
-        ('available', 'Available'),
-        ('on_loan', 'On Loan'),
-        ('reserved', 'Reserved'),
-    ], default='available')
+    imprint = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True,
+        help_text="Enter the publisher imprint"
+    )
+    status = models.CharField(
+        max_length=1,
+        choices=[
+            (
+                LoanStatus.MAINTENANCE.value,
+                _(str(LoanStatus.MAINTENANCE.name).capitalize())
+            ),
+            (
+                LoanStatus.ON_LOAN.value,
+                _(str(LoanStatus.ON_LOAN.name).capitalize())
+            ),
+            (
+                LoanStatus.AVAILABLE.value,
+                _(str(LoanStatus.AVAILABLE.name).capitalize())
+            ),
+            (
+                LoanStatus.RESERVED.value,
+                _(str(LoanStatus.RESERVED.name).capitalize())
+            ),
+        ],
+        blank=True,
+        default=LoanStatus.MAINTENANCE.value,
+        help_text=_('Book availability')
+    )
 
     def __str__(self):
         return f'{self.id} ({self.book.title})'
-    
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,58 @@
-# catalog/views.py
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from catalog.models import Book, Author, BookInstance
+from catalog.constants import LoanStatus, DEFAULT_PAGINATION_SIZE
+
 
 def index(request):
-    return HttpResponse("Welcome to the Catalog!")
+    """View function for home page of site."""
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(
+        status__exact=LoanStatus.AVAILABLE.value).count()
+    num_authors = Author.objects.count()
+
+    num_visits = request.session.get('num_visits', 0)
+    num_visits = num_visits + 1
+    request.session['num_visits'] = num_visits
+    request.session.modified = True
+
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = DEFAULT_PAGINATION_SIZE
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+def book_detail_view(request, primary_key):
+    book = get_object_or_404(Book, pk=primary_key)
+    book_instances = book.book_instance_set.select_related().all()
+
+    context = {
+        'book': book,
+        'book_instances': book_instances,
+    }
+    return render(
+        request,
+        'catalog/book_detail.html',
+        context=context
+    )
