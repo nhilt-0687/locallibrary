5b4a811240c66cfebf6b10e8211a3084b427b144
itsnhyyy <22521038@gm.uit.edu.vn>
2025-07-11 21:52:26 +0700

    Django Tutorial Part 6: Generic list and detail views
    f

--- catalog/constants.py
+++ catalog/constants.py
@@ -1,6 +1,18 @@
-# locallibrary/constants.py
+from enum import Enum
+<<<<<<< HEAD
+
+=======
+>>>>>>> a2457d3 (Django Tutorial Part 5: Creating Our Home Page (#9))
 MAX_LENGTH_NAME = 200
 MAX_LENGTH_AUTHOR_NAME = 100
 MAX_LENGTH_ISBN = 13
 MAX_LENGTH_SUMMARY = 1000
 MAX_LENGTH_UNIQUE_ID = 20
+
+class LoanStatus(Enum):
+    MAINTENANCE = 'm'
+    ON_LOAN = 'o'
+    AVAILABLE = 'a'
+    RESERVED = 'r'
+
+DEFAULT_IMPRINT = 'Penguin Group'
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,53 @@
-# catalog/views.py
-from django.http import HttpResponse
+from django.shortcuts import render
+<<<<<<< HEAD
+from django.shortcuts import get_object_or_404
+from django.views import generic
+from catalog.models import Book, Author, BookInstance, Genre
+from catalog.constants import LoanStatus
+
 
 def index(request):
-    return HttpResponse("Welcome to the Catalog!")
+    """View function for home page of site."""
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact=LoanStatus.AVAILABLE).count()
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
+=======
+from django.views import generic
+from catalog.models import Book, Author, BookInstance, Genre
+from catalog.constants import LoanStatus
+
+def index(request):
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_instances_available = BookInstance.objects.filter(status__exact=LoanStatus.AVAILABLE.value).count()
+    num_authors = Author.objects.count()
+>>>>>>> a2457d3 (Django Tutorial Part 5: Creating Our Home Page (#9))
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
+<<<<<<< HEAD
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+def book_detail_view(request, primary_key):
+    book = get_object_or_404(Book, pk=primary_key)
+    return render(request, 'catalog/book_detail.html', context={'book': book})
+=======
+>>>>>>> a2457d3 (Django Tutorial Part 5: Creating Our Home Page (#9))
