dfb4ecb3626d63601f1c2e0712a238a84eda38b8
itsnhyyy <22521038@gm.uit.edu.vn>
2025-07-11 21:52:26 +0700

    Django Tutorial Part 6: Generic list and detail views
    f

--- catalog/constants.py
+++ catalog/constants.py
@@ -1,6 +1,20 @@
-# locallibrary/constants.py
+from enum import Enum
+
 MAX_LENGTH_NAME = 200
 MAX_LENGTH_AUTHOR_NAME = 100
 MAX_LENGTH_ISBN = 13
 MAX_LENGTH_SUMMARY = 1000
 MAX_LENGTH_UNIQUE_ID = 20
+
+# Pagination constants
+DEFAULT_PAGINATION_SIZE = 10
+
+
+class LoanStatus(Enum):
+    MAINTENANCE = 'm'
+    ON_LOAN = 'o'
+    AVAILABLE = 'a'
+    RESERVED = 'r'
+
+
+DEFAULT_IMPRINT = 'Penguin Group'
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,58 @@
-# catalog/views.py
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from catalog.models import Book, Author, BookInstance
+from catalog.constants import LoanStatus, DEFAULT_PAGINATION_SIZE
+
 
 def index(request):
-    return HttpResponse("Welcome to the Catalog!")
+    """View function for home page of site."""
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(
+        status__exact=LoanStatus.AVAILABLE
+    ).count()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = DEFAULT_PAGINATION_SIZE
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['book_instances'] = \
+            self.object.bookinstance_set.select_related().all()
+        return context
+
+
+def book_detail_view(request, primary_key):
+    book = get_object_or_404(Book, pk=primary_key)
+    book_instances = book.bookinstance_set.select_related().all()
+
+    context = {
+        'book': book,
+        'book_instances': book_instances,
+    }
+    return render(
+        request,
+        'catalog/book_detail.html',
+        context=context
+    )
